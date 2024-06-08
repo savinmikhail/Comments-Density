ce5b339868280b233e46c1dff3efc7996f8134f5
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-08 17:41:14 +0700

    feat: composer plugin

---
+++ src/Composer/CommentsDensityPlugin.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\CommentsDensity\Composer;
+
+use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\OperationInterface;
+use Composer\DependencyResolver\Operation\UninstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+final class CommentsDensityPlugin implements  PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        // No activation logic needed
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        // No deactivation logic needed
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        // No uninstallation logic needed
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'promptForPreCommitHook',
+            ScriptEvents::POST_UPDATE_CMD => 'promptForPreCommitHook',
+        ];
+    }
+
+    public static function promptForPreCommitHook(Event $event): void
+    {
+        $io = $event->getIO();
+        $io->write('Run pre-commit installation');
+        $shouldInstallHook = $io->askConfirmation('Do you want to install the pre-commit hook? [y/N] ', false);
+
+        if ($shouldInstallHook) {
+            $io->write('Installing pre-commit hook...');
+            copy('vendor/savinmikhail/comments-density/commit_density_pre-commit.sh', '.git/hooks/pre-commit');
+            chmod('.git/hooks/pre-commit', 0755);
+            $io->write('Pre-commit hook installed.');
+            return;
+        }
+
+        $io->write('Pre-commit hook installation skipped.');
+    }
+}
