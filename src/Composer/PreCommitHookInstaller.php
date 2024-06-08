c40eab1ca4bfad30ce18a41ef27c8be1c31eaa49
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-08 16:09:54 +0700

    refactor: pre commit hook installation

--- src/Composer/PreCommitHookInstaller.php
+++ src/Composer/PreCommitHookInstaller.php
@@ -6,17 +6,18 @@ use Composer\Script\Event;
 
 final readonly class PreCommitHookInstaller
 {
-    public static function promptForPreCommitHook(Event $event)
+    public static function promptForPreCommitHook(Event $event): void
     {
         $io = $event->getIO();
-
-        if ($io->askConfirmation('Do you want to install the pre-commit hook? [y/N] ', false)) {
+        $io->write('Run pre-commit installation');
+        $shouldInstallHook = $io->askConfirmation('Do you want to install the pre-commit hook? [y/N] ', false);
+        if ($shouldInstallHook) {
             $io->write('Installing pre-commit hook...');
             copy('vendor/savinmikhail/comments-density/commit_density_pre-commit.sh', '.git/hooks/pre-commit');
             chmod('.git/hooks/pre-commit', 0755);
             $io->write('Pre-commit hook installed.');
-        } else {
-            $io->write('Pre-commit hook installation skipped.');
+            return;
         }
+        $io->write('Pre-commit hook installation skipped.');
     }
 }
