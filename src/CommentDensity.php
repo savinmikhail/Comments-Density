5a0a070d94d90ed4f25120e7e3f87216eb833b44
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-09 20:04:38 +0700

    refactor: comment density to use reporters

--- src/CommentDensity.php
+++ src/CommentDensity.php
@@ -8,13 +8,17 @@ use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use SavinMikhail\CommentsDensity\Comments\CommentFactory;
 use SavinMikhail\CommentsDensity\Comments\CommentTypeInterface;
+use SavinMikhail\CommentsDensity\DTO\CdsDTO;
+use SavinMikhail\CommentsDensity\DTO\CommentDTO;
+use SavinMikhail\CommentsDensity\DTO\CommentStatisticsDTO;
+use SavinMikhail\CommentsDensity\DTO\ComToLocDTO;
+use SavinMikhail\CommentsDensity\DTO\OutputDTO;
+use SavinMikhail\CommentsDensity\DTO\PerformanceMetricsDTO;
+use SavinMikhail\CommentsDensity\Reporters\ConsoleReporter;
+use SavinMikhail\CommentsDensity\Reporters\HtmlReporter;
 use SplFileInfo;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\Table;
 
-use function array_keys;
-use function array_map;
-use function array_sum;
 use function round;
 
 final class CommentDensity
@@ -61,29 +65,168 @@ final class CommentDensity
         $endTime = microtime(true);
         $executionTimeMS = round(($endTime - $startTime) * 1000, 2);
         $peakMemoryUsage = memory_get_peak_usage(true);
-        if (! empty($this->outputConfig) && $this->outputConfig['type'] === 'html') {
-            $this->generateHtmlOutput(
-                $commentStatistics,
-                $totalLinesOfCode,
-                $cdsSum / $filesAnalyzed,
-                $comments,
-                $executionTimeMS,
-                $peakMemoryUsage,
-                $filesAnalyzed
-            );
-            $this->output->writeln('<info>Html report was generated successfully!</info>');
-            return $this->exceedThreshold;
-        }
-        $this->printStatistics(
+
+        $outputDTO = $this->createOutputDTO(
+            $comments,
             $commentStatistics,
             $totalLinesOfCode,
-            $cdsSum / $filesAnalyzed,
+            $cdsSum / $totalLinesOfCode,
             $filesAnalyzed,
-            $comments
+            $executionTimeMS,
+            $peakMemoryUsage
         );
-        $this->printPerformanceMetrics($executionTimeMS, $peakMemoryUsage);
+
+        $reporter = new ConsoleReporter($this->output, $outputDTO);
+        if (! empty($this->outputConfig) && $this->outputConfig['type'] === 'html') {
+            $reporter = new HtmlReporter($outputDTO, __DIR__ . '/../../../' . $this->outputConfig['file']);
+        }
+        $reporter->report();
 
         return $this->exceedThreshold;
+    }
+
+    private function createOutputDTO(
+        array $comments,
+        array $commentStatistics,
+        int $linesOfCode,
+        float $cds,
+        int $filesAnalyzed,
+        float $executionTime,
+        float $peakMemoryUsage
+    ): OutputDTO {
+        $performanceMetricsDTO = new PerformanceMetricsDTO(
+            $executionTime,
+            round($peakMemoryUsage / 1024 / 1024, 2)
+        );
+        return new OutputDTO(
+            $filesAnalyzed,
+            $this->prepareCommentStatistics($commentStatistics),
+            $this->prepareComments($comments),
+            $performanceMetricsDTO,
+            $this->prepareComToLoc($commentStatistics, $linesOfCode),
+            $this->prepareCDS($cds)
+        );
+    }
+
+    private function prepareCDS(float $cds): CdsDTO
+    {
+        $cds = round($cds, 2);
+        return new CdsDTO(
+            $cds,
+            $this->getColorForCDS($cds),
+        );
+    }
+
+    private function getColorForCDS(float $cds): string
+    {
+        if (! isset($this->thresholds['CDS'])) {
+            return 'white';
+        }
+        if ($cds >= $this->thresholds['CDS']) {
+            return 'green';
+        }
+        $this->exceedThreshold = true;
+        return 'red';
+    }
+
+    private function prepareComToLoc(array $commentStatistics, int $linesOfCode): ComToLocDTO
+    {
+        $ratio = $this->getRatio($commentStatistics, $linesOfCode);
+        return new ComToLocDTO(
+            $ratio,
+            $this->getColorForRatio($ratio)
+        );
+    }
+
+    private function getRatio(array $commentStatistics, int $linesOfCode): float
+    {
+        $totalComments = array_sum($commentStatistics);
+        return round($totalComments / $linesOfCode, 2);
+    }
+
+    private function getColorForRatio(float $ratio): string
+    {
+        if (! isset($this->thresholds['Com/LoC'])) {
+            return 'white';
+        }
+        if ($ratio >= $this->thresholds['Com/LoC']) {
+            return 'green';
+        }
+        $this->exceedThreshold = true;
+        return 'red';
+    }
+
+    private function prepareCommentStatistics(array $commentStatistics): array
+    {
+        $preparedCommentStatistics = [];
+        foreach ($commentStatistics as $type => $count) {
+            if ($type === 'missingDocblock') {
+                $preparedCommentStatistics[] = new CommentStatisticsDTO(
+                    $this->getMissingDocBlockColor(),
+                    'missingDocblock',
+                    $count,
+                    $this->getMissingDocBlockStatColor($count)
+                );
+                continue;
+            }
+            $commentType = $this->commentFactory->getCommentType($type);
+            if ($commentType) {
+                $preparedCommentStatistics[] = new CommentStatisticsDTO(
+                    $commentType->getColor(),
+                    $commentType->getName(),
+                    $count,
+                    $commentType->getStatColor($count, $this->thresholds)
+                );
+            }
+        }
+        return  $preparedCommentStatistics;
+    }
+
+    private function getMissingDocBlockColor(): string
+    {
+        return 'red';
+    }
+
+    private function getMissingDocBlockStatColor(float $count): string
+    {
+        if (! isset($this->thresholds['missingDocBlock'])) {
+            return 'white';
+        }
+        if ($count <= $this->thresholds['missingDocBlock']) {
+            return 'green';
+        }
+        $this->exceedThreshold = true;
+        return 'red';
+    }
+
+    private function prepareComments(array $comments): array
+    {
+        $preparedComments = [];
+        foreach ($comments as $comment) {
+            /** @var CommentTypeInterface|string $commentType */
+            $commentType = $comment['type'];
+            if ($commentType === 'missingDocblock') {
+                $preparedComments[] = new CommentDTO(
+                    'missingDocblock',
+                    'red',
+                    $comment['file'],
+                    $comment['line'],
+                    $comment['content']
+                );
+                continue;
+            }
+            if ($commentType->getAttitude() === 'good') {
+                continue;
+            }
+            $preparedComments[] = new CommentDTO(
+                $commentType->getName(),
+                $commentType->getColor(),
+                $comment['file'],
+                $comment['line'],
+                $comment['content']
+            );
+        }
+        return $preparedComments;
     }
 
     public function analyzeFile(string $filename): bool
@@ -101,21 +244,6 @@ final class CommentDensity
             $cdsSum
         );
 
-        if (! empty($this->outputConfig) && $this->outputConfig['type'] === 'html') {
-            $this->generateHtmlOutput(
-                $commentStatistics,
-                $totalLinesOfCode,
-                $cdsSum,
-                $comments,
-                0,
-                0,
-                1
-            );
-            $this->output->writeln('<info>Html report was generated successfully!</info>');
-            return $this->exceedThreshold;
-        }
-
-        $this->printStatistics($commentStatistics, $totalLinesOfCode, $cdsSum, 1, $comments);
 
         return $this->exceedThreshold;
     }
@@ -128,232 +256,5 @@ final class CommentDensity
             }
         }
         return false;
-    }
-
-    private function generateHtmlOutput(
-        array $commentStatistics,
-        int $linesOfCode,
-        float $cds,
-        array $comments,
-        float $executionTime,
-        int $peakMemoryUsage,
-        int $filesAnalyzed,
-    ): void {
-        $time = $executionTime . ' ms';
-        $memory = round($peakMemoryUsage / 1024 / 1024, 2) . 'MB';
-        $cds = round($cds, 2);
-
-        $html = "<html><head><title>Comment Density Report</title></head><body>";
-        $html .= "<h1>Comment Density Report</h1>";
-        $html .= "<p><strong>Execution Time:</strong> $time</p>";
-        $html .= "<p><strong>Peak Memory Usage:</strong> $memory</p>";
-        $html .= "<p><strong>CDS:</strong> $cds</p>";
-        $html .= "<p><strong>Com/LoC:</strong> {$this->getRatio($commentStatistics, $linesOfCode)}</p>";
-        $html .= "<p><strong>Files analyzed:</strong> $filesAnalyzed</p>";
-
-        $html .= "<h2>Comment Statistics</h2>";
-        $html .= "<table border='1'><tr><th>Comment Type</th><th>Lines</th></tr>";
-        foreach ($commentStatistics as $type => $count) {
-            $comment = $this->commentFactory->getCommentType($type);
-            if (!$comment) {
-                $color = $this->getMissingDocBlockColor();
-                $statColor = $this->getMissingDocBlockStatColor($count);
-                $html .= "<tr><td style='color: $color;'>$type</td><td style='color: $statColor;'>$count</td></tr>";
-                continue;
-            }
-            $color = $comment->getColor();
-            $statColor = $comment->getStatColor($count, $this->thresholds);
-
-            $html .= "<tr><td style='color: $color;'>$type</td><td style='color: $statColor;'>$count</td></tr>";
-        }
-        $html .= "</table>";
-
-        $html .= "<h2>Detailed Comments</h2>";
-        $html .= "<table border='1'><tr><th>Type</th><th>File</th><th>Line</th><th>Content</th></tr>";
-        foreach ($comments as $comment) {
-            /** @var CommentTypeInterface|string $commentType */
-            $commentType = $comment['type'];
-            if ($commentType === 'missingDocblock') {
-                $typeColor = $this->getMissingDocBlockColor();
-                $fileOutput = htmlspecialchars($comment['file']);
-                $lineOutput = htmlspecialchars((string)$comment['line']);
-                $contentOutput = htmlspecialchars($comment['content']);
-                $html .= "<tr>
-                    <td style='color: $typeColor;'>{$comment['type']}</td>
-                    <td>$fileOutput</td>
-                    <td>$lineOutput</td>
-                    <td>$contentOutput</td>
-                </tr>";
-                continue;
-            }
-            if ($commentType->getAttitude() === 'good') {
-                continue;
-            }
-            $typeColor = $commentType->getColor();
-            $fileOutput = htmlspecialchars($comment['file']);
-            $lineOutput = htmlspecialchars((string)$comment['line']);
-            $contentOutput = htmlspecialchars($comment['content']);
-            $html .= "<tr>
-                <td style='color: $typeColor;'>{$comment['type']}</td>
-                <td>$fileOutput</td>
-                <td>$lineOutput</td>
-                <td>$contentOutput</td>
-            </tr>";
-        }
-        $html .= "</table>";
-
-        $html .= "</body></html>";
-
-        file_put_contents(__DIR__ . '/../../' . $this->outputConfig['file'], $html);
-    }
-
-    private function printStatistics(
-        array $commentStatistics,
-        int $linesOfCode,
-        float $cds,
-        int $filesAnalyzed,
-        array $comments
-    ): void {
-        $this->printDetailedComments($comments);
-        $this->printTable($commentStatistics);
-        $this->printComToLoc($commentStatistics, $linesOfCode);
-        $this->printCDS($cds);
-        $this->printFilesAnalyzed($filesAnalyzed);
-    }
-
-    private function printFilesAnalyzed(int $filesAnalyzed): void
-    {
-        $this->output->writeln("<fg=white>Files analyzed: $filesAnalyzed</>");
-    }
-
-    private function printDetailedComments(array $comments): void
-    {
-        foreach ($comments as $comment) {
-            /** @var CommentTypeInterface|string $commentType */
-            $commentType = $comment['type'];
-            if ($commentType === 'missingDocblock') {
-                $this->output->writeln(
-                    "<fg=red>$commentType comment</> in "
-                    . "<fg=blue>{$comment['file']}</>:"
-                    . "<fg=blue>{$comment['line']}</>    "
-                    . "{$comment['content']}"
-                );
-                continue;
-            }
-            if ($commentType->getAttitude() === 'good') {
-                continue;
-            }
-            $this->output->writeln(
-                "<fg={$commentType->getColor()}>{$commentType->getName()} comment</> in "
-                . "<fg=blue>{$comment['file']}</>:"
-                . "<fg=blue>{$comment['line']}</>    "
-                . "<fg=yellow>{$comment['content']}</>"
-            );
-        }
-    }
-
-    private function printPerformanceMetrics(float $executionTime, int $peakMemoryUsage): void
-    {
-        $memory = round($peakMemoryUsage / 1024 / 1024, 2);
-        $this->output->writeln("<fg=white>Time: $executionTime ms, Memory: {$memory}MB</>");
-    }
-
-    private function getRatio(array $commentStatistics, int $linesOfCode): float
-    {
-        $totalComments = array_sum($commentStatistics);
-        return round($totalComments / $linesOfCode, 2);
-    }
-
-    private function getColorForCDS(float $cds): string
-    {
-        if (! isset($this->thresholds['CDS'])) {
-            return 'white';
-        }
-        if ($cds >= $this->thresholds['CDS']) {
-            return 'green';
-        }
-        $this->exceedThreshold = true;
-        return 'red';
-    }
-
-    private function getColorForRatio(float $ratio): string
-    {
-        if (! isset($this->thresholds['Com/LoC'])) {
-            return 'white';
-        }
-        if ($ratio >= $this->thresholds['Com/LoC']) {
-            return 'green';
-        }
-        $this->exceedThreshold = true;
-        return 'red';
-    }
-
-    /**
-     * @param array $commentStatistics
-     * @return void
-     */
-    private function printTable(array $commentStatistics): void
-    {
-        $table = new Table($this->output);
-        $table
-            ->setHeaders(['Comment Type', 'Lines'])
-            ->setRows(
-                array_map(function (string $type, int $count): array {
-                    if ($type === 'missingDocblock') {
-                        $color = $this->getMissingDocBlockStatColor($count);
-                        return [
-                            "<fg=" . $this->getMissingDocBlockColor() . ">$type</>", "<fg=$color>$count</>"
-                        ];
-                    }
-                    $commentType = $this->commentFactory->getCommentType($type);
-                    if ($commentType) {
-                        $color = $commentType->getStatColor($count, $this->thresholds);
-                        return ["<fg=" . $commentType->getColor() . ">$type</>", "<fg=$color>$count</>"];
-                    }
-                    return [$type, $count];
-                }, array_keys($commentStatistics), $commentStatistics)
-            );
-
-        $table->render();
-    }
-
-    private function getMissingDocBlockStatColor(float $count): string
-    {
-        if (! isset($this->thresholds['missingDocBlock'])) {
-            return 'white';
-        }
-        if ($count <= $this->thresholds['missingDocBlock']) {
-            return 'green';
-        }
-        $this->exceedThreshold = true;
-        return 'red';
-    }
-
-    private function getMissingDocBlockColor(): string
-    {
-        return 'red';
-    }
-
-    /**
-     * @param array $commentStatistics
-     * @param int $linesOfCode
-     * @return void
-     */
-    private function printComToLoc(array $commentStatistics, int $linesOfCode): void
-    {
-        $ratio = $this->getRatio($commentStatistics, $linesOfCode);
-        $color = $this->getColorForRatio($ratio);
-        $this->output->writeln(["<fg=$color>Com/LoC: $ratio</>"]);
-    }
-
-    /**
-     * @param float $cds
-     * @return void
-     */
-    private function printCDS(float $cds): void
-    {
-        $cds = round($cds, 2);
-        $color = $this->getColorForCDS($cds);
-        $this->output->writeln(["<fg=$color>CDS: $cds</>"]);
     }
 }
