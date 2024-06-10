2c9400579942c382fdaeb355b44d652994f8faae
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-10 12:10:17 +0700

    test: cds test

---
+++ tests/CDSTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\Tests\CommentsDensity;
+
+use PHPUnit\Framework\TestCase;
+use SavinMikhail\CommentsDensity\CDS;
+use SavinMikhail\CommentsDensity\Comments\CommentFactory;
+
+final class CDSTest  extends TestCase
+{
+    public function testCalculateCDS(): void
+    {
+        $commentFactory = new CommentFactory();
+        $calculator = new CDS([], $commentFactory);
+
+        $commentStatistics = [
+            'docBlock' => 10,
+            'regular' => 5,
+            'todo' => 2,
+            'fixme' => 1,
+            'license' => 1,
+            'missingDocblock' => 3,
+        ];
+
+        $cds = $calculator->calculateCDS($commentStatistics);
+        $this->assertIsFloat($cds);
+        $this->assertGreaterThanOrEqual(0, $cds);
+        $this->assertLessThanOrEqual(1, $cds);
+    }
+}
