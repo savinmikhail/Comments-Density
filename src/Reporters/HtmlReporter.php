b963febb7ba4b4c36fd9a1f2b5f92dfa2e2d2005
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-06-10 10:09:52 +0700

    refactor: html reporter

--- src/Reporters/HtmlReporter.php
+++ src/Reporters/HtmlReporter.php
@@ -9,26 +9,52 @@ final readonly class HtmlReporter implements ReporterInterface
     public function __construct(private string $reportPath)
     {
     }
-
     public function report(OutputDTO $dto): void
     {
         $html = "<html><head><title>Comment Density Report</title></head><body>";
-        $html .= "<h1>Comment Density Report</h1>";
-        $html .= "<p><strong>Execution Time:</strong> {$dto->performanceMetricsDTO->executionTime} ms</p>";
-        $html .= "<p><strong>Peak Memory Usage:</strong> {$dto->performanceMetricsDTO->peakMemoryUsage} MB</p>";
-        $html .= "<p><strong>CDS:</strong> {$dto->cdsDTO->cds}</p>";
-        $html .= "<p><strong>Com/LoC:</strong> {$dto->comToLocDTO->comToLoc}</p>";
-        $html .= "<p><strong>Files analyzed:</strong> {$dto->filesAnalyzed}</p>";
+        $html .= $this->generateHeader($dto);
+        $html .= $this->generateCommentStatisticsTable($dto);
+        $html .= $this->generateDetailedCommentsTable($dto);
+        $html .= "</body></html>";
 
-        $html .= "<h2>Comment Statistics</h2>";
+        file_put_contents($this->reportPath, $html);
+    }
+
+    private function generateHeader(OutputDTO $dto): string
+    {
+        return "<h1>Comment Density Report</h1>"
+            . "<p><strong>Execution Time:</strong> {$dto->performanceDTO->executionTime} ms</p>"
+            . "<p><strong>Peak Memory Usage:</strong> {$dto->performanceDTO->peakMemoryUsage} MB</p>"
+            . "<p><strong>CDS:</strong> {$dto->cdsDTO->cds}</p>"
+            . "<p><strong>Com/LoC:</strong> {$dto->comToLocDTO->comToLoc}</p>"
+            . "<p><strong>Files analyzed:</strong> {$dto->filesAnalyzed}</p>";
+    }
+
+    private function generateCommentStatisticsTable(OutputDTO $dto): string
+    {
+        $html = "<h2>Comment Statistics</h2>";
         $html .= "<table border='1'><tr><th>Comment Type</th><th>Lines</th></tr>";
         foreach ($dto->commentsStatistics as $commentStatisticsDTO) {
-            $html .= "<tr><td style='color: {$commentStatisticsDTO->typeColor};'>{$commentStatisticsDTO->type}</td><td style='color: {$commentStatisticsDTO->typeColor};'>{$commentStatisticsDTO->count}</td></tr>";
+            $html .= "<tr>
+                    <td style='color: {$commentStatisticsDTO->typeColor};'>{$commentStatisticsDTO->type}</td>
+                    <td style='color: {$commentStatisticsDTO->typeColor};'>{$commentStatisticsDTO->count}</td>
+                </tr>";
         }
         $html .= "</table>";
 
-        $html .= "<h2>Detailed Comments</h2>";
-        $html .= "<table border='1'><tr><th>Type</th><th>File</th><th>Line</th><th>Content</th></tr>";
+        return $html;
+    }
+
+    private function generateDetailedCommentsTable(OutputDTO $dto): string
+    {
+        $html = "<h2>Detailed Comments</h2>";
+        $html .= "<table border='1'>
+            <tr>
+                <th>Type</th>
+                <th>File</th>
+                <th>Line</th>
+                <th>Content</th>
+            </tr>";
         foreach ($dto->comments as $comment) {
             $commentType = htmlspecialchars($comment->commentType);
             $commentTypeColor = htmlspecialchars($comment->commentTypeColor);
@@ -37,7 +63,7 @@ final readonly class HtmlReporter implements ReporterInterface
             $content = htmlspecialchars($comment->content);
 
             $html .= "<tr>
-                <td style='color: $commentTypeColor;'>$commentType}</td>
+                <td style='color: $commentTypeColor;'>$commentType</td>
                 <td>$file</td>
                 <td>$line</td>
                 <td>$content</td>
@@ -45,8 +71,6 @@ final readonly class HtmlReporter implements ReporterInterface
         }
         $html .= "</table>";
 
-        $html .= "</body></html>";
-
-        file_put_contents($this->reportPath, $html);
+        return $html;
     }
 }
