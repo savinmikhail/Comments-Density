41f6292fa0e9151b7580a1432aae57b147f29fba
Mikhail <salazar290720035017@gmail.com>
2024-06-28 15:03:04 +0700

    refactor(analize comments command): extend from custom command

--- src/Commands/AnalyzeCommentCommand.php
+++ src/Commands/AnalyzeCommentCommand.php
@@ -4,23 +4,9 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\Commands;
 
-use SavinMikhail\CommentsDensity\CommentDensity;
-use SavinMikhail\CommentsDensity\Comments\CommentFactory;
-use SavinMikhail\CommentsDensity\DTO\Input\ConfigDTO;
-use SavinMikhail\CommentsDensity\FileAnalyzer;
-use SavinMikhail\CommentsDensity\Metrics\CDS;
-use SavinMikhail\CommentsDensity\Metrics\ComToLoc;
-use SavinMikhail\CommentsDensity\Metrics\Metrics;
-use SavinMikhail\CommentsDensity\Metrics\PerformanceMonitor;
-use SavinMikhail\CommentsDensity\MissingDocBlockAnalyzer;
 use SavinMikhail\CommentsDensity\Reporters\ReporterFactory;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Parser;
-
-use function array_map;
-use function dirname;
 
 class AnalyzeCommentCommand extends Command
 {
@@ -36,81 +22,13 @@ class AnalyzeCommentCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $configFile = $this->getProjectRoot() . '/comments_density.yaml';
-        $config = $this->parseConfigFile($configFile);
+        $configDto = $this->getConfigDto();
 
-        $directories = $this->getDirectories($config);
-        $exclude = $this->getExcludes($config);
-        $thresholds = $config['thresholds'] ?? [];
-        $outputConfig = $config['output'] ?? [];
+        $files = $this->getFilesFromDirectories($this->getDirectories($configDto));
+        $reporter = (new ReporterFactory())->createReporter($output, $configDto);
 
-        $configDto = new ConfigDTO(
-            $thresholds,
-            $exclude,
-            $outputConfig
-        );
+        $analyzer = $this->getAnalyzer($configDto, $output, $reporter);
 
-        $reporterFactory = new ReporterFactory();
-        $commentFactory = new CommentFactory();
-        $missingDocBlock = new MissingDocBlockAnalyzer();
-        $cds = new CDS($configDto->thresholds, $commentFactory);
-        $fileAnalyzer = new FileAnalyzer(
-            $output,
-            $missingDocBlock,
-            $cds,
-            $commentFactory
-        );
-
-        $metrics = new Metrics($cds, new ComToLoc($configDto->thresholds), new PerformanceMonitor());
-        $analyzer = new CommentDensity(
-            $configDto,
-            $commentFactory,
-            $fileAnalyzer,
-            $reporterFactory->createReporter($output, $configDto),
-            $missingDocBlock,
-            $metrics
-        );
-
-        return $this->analyze($analyzer, $directories, $output);
-    }
-
-    protected function parseConfigFile(string $configFile): array
-    {
-        $yamlParser = new Parser();
-        return $yamlParser->parseFile($configFile);
-    }
-
-    protected function getDirectories(array $config): array
-    {
-        return array_map(
-            fn($dir) => $this->getProjectRoot() . '/' . $dir,
-            $config['directories']
-        );
-    }
-
-    protected function getExcludes(array $config): array
-    {
-        return array_map(
-            fn($dir) => $this->getProjectRoot() . '/' . $dir,
-            $config['exclude']
-        );
-    }
-
-    protected function analyze(CommentDensity $analyzer, array $directories, OutputInterface $output): int
-    {
-        $limitExceeded = $analyzer->analyzeDirectories($directories);
-
-        if ($limitExceeded) {
-            $output->writeln('<error>Comment thresholds were exceeded!</error>');
-            return Command::FAILURE;
-        }
-
-        $output->writeln('<info>Comment thresholds are passed!</info>');
-        return Command::SUCCESS;
-    }
-
-    protected function getProjectRoot(): string
-    {
-        return dirname(__DIR__, 5);
+        return $this->analyze($analyzer, $files, $output);
     }
 }
