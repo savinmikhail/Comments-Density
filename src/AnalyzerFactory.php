dcdb43f879516c41eb901fdddbfa1a23078dc5a0
Mikhail <salazar290720035017@gmail.com>
2024-06-28 16:05:17 +0700

    feat: analyzer factory

---
+++ src/AnalyzerFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\CommentsDensity;
+
+use SavinMikhail\CommentsDensity\Comments\CommentFactory;
+use SavinMikhail\CommentsDensity\DTO\Input\ConfigDTO;
+use SavinMikhail\CommentsDensity\Metrics\CDS;
+use SavinMikhail\CommentsDensity\Metrics\ComToLoc;
+use SavinMikhail\CommentsDensity\Metrics\Metrics;
+use SavinMikhail\CommentsDensity\Metrics\PerformanceMonitor;
+use SavinMikhail\CommentsDensity\Reporters\ReporterInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final readonly class AnalyzerFactory
+{
+    public function getAnalyzer(
+        ConfigDTO $configDto,
+        OutputInterface $output,
+        ReporterInterface $reporter
+    ): CommentDensity {
+        $commentFactory = new CommentFactory();
+        $missingDocBlock = new MissingDocBlockAnalyzer();
+        $cds = new CDS($configDto->thresholds, $commentFactory);
+
+        $fileAnalyzer = new FileAnalyzer(
+            $output,
+            $missingDocBlock,
+            $cds,
+            $commentFactory
+        );
+
+        $metrics = new Metrics(
+            $cds,
+            new ComToLoc($configDto->thresholds),
+            new PerformanceMonitor()
+        );
+
+        return new CommentDensity(
+            $configDto,
+            $commentFactory,
+            $fileAnalyzer,
+            $reporter,
+            $missingDocBlock,
+            $metrics
+        );
+    }
+}
