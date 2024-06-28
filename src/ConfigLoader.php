823f4e16ac765041ad26a7aba41438f89df902cc
Mikhail <salazar290720035017@gmail.com>
2024-06-28 15:58:11 +0700

    feat: config loader

---
+++ src/ConfigLoader.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\CommentsDensity;
+
+use SavinMikhail\CommentsDensity\DTO\Input\ConfigDTO;
+use Symfony\Component\Yaml\Parser;
+
+use function array_map;
+use function dirname;
+
+use const DIRECTORY_SEPARATOR;
+
+final readonly class ConfigLoader
+{
+    private string $projectRoot;
+    protected const CONFIG_FILE = 'comments_density.yaml';
+
+    protected function parseConfigFile(string $configFile): array
+    {
+        $yamlParser = new Parser();
+        return $yamlParser->parseFile($configFile);
+    }
+
+    public function getConfigDto(): ConfigDTO
+    {
+        $config = $this->getConfig();
+        $exclude = $this->getExcludes($config);
+        $thresholds = $config['thresholds'];
+        $outputConfig = $config['output'];
+        $directories = $this->getDirectories($config);
+
+        return new ConfigDTO(
+            $thresholds,
+            $exclude,
+            $outputConfig,
+            $directories,
+        );
+    }
+
+    protected function getConfig(): array
+    {
+        $configFile = $this->getProjectRoot() . DIRECTORY_SEPARATOR . self::CONFIG_FILE;
+        return $this->parseConfigFile($configFile);
+    }
+
+    protected function getExcludes(array $config): array
+    {
+        return array_map(
+            fn($dir) => $this->getProjectRoot() . '/' . $dir,
+            $config['exclude']
+        );
+    }
+
+    protected function getDirectories(array $config): array
+    {
+        return array_map(
+            fn($dir) => $this->getProjectRoot() . '/' . $dir,
+            $config['directories']
+        );
+    }
+
+    protected function getProjectRoot(): string
+    {
+        if (! $this->projectRoot) {
+            $this->projectRoot = dirname(__DIR__, 5);
+        }
+        return $this->projectRoot;
+    }
+}
