8242bfcbf95be368136e96297130cb15dbe1550c
Mikhail <salazar290720035017@gmail.com>
2024-06-29 15:07:33 +0700

    feat: rector

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([DowngradeLevelSetList::DOWN_TO_PHP_74]);
