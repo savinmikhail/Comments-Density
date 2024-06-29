94d2543b7a245608783f76541440733c7f4d0e3b
Mikhail <salazar290720035017@gmail.com>
2024-06-29 19:39:05 +0700

    feat(comment factory): allowed types list

--- src/Comments/CommentFactory.php
+++ src/Comments/CommentFactory.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\Comments;
 
+use function in_array;
+
 class CommentFactory
 {
     /** @var array<array-key, CommentTypeInterface> */
     private array $commentTypes;
+    private array $allowedTypes;
 
-    public function __construct()
+    public function __construct(array $allowedTypes = [])
     {
         $this->commentTypes =  [
             new TodoComment(),
@@ -18,6 +21,8 @@ class CommentFactory
             new LicenseComment(),
             new DocBlockComment(),
         ];
+
+        $this->allowedTypes = $allowedTypes;
     }
 
     public function getCommentType(string $name): ?CommentTypeInterface
@@ -33,7 +38,13 @@ class CommentFactory
     public function classifyComment(string $token): ?CommentTypeInterface
     {
         foreach ($this->commentTypes as $commentType) {
-            if ($commentType->matchesPattern($token)) {
+            if (! $commentType->matchesPattern($token)) {
+                continue;
+            }
+            if (
+                empty($this->allowedTypes)
+                || in_array($commentType->getName(), $this->allowedTypes, true)
+            ) {
                 return $commentType;
             }
         }
