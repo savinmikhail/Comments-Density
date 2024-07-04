dfc5c99923d8160a04bffc20565896b771abeb09
Mikhail <salazar290720035017@gmail.com>
2024-07-04 08:51:09 +0700

    fix: default file generation

--- src/Composer/CommentsDensityPlugin.php
+++ src/Composer/CommentsDensityPlugin.php
@@ -18,31 +18,35 @@ use function file_put_contents;
 
 final class CommentsDensityPlugin implements PluginInterface, EventSubscriberInterface
 {
-    private const CONFIG = [
-        'directories' => [
-            'src',
-        ],
-        'exclude' => [
-            'src/DTO',
-        ],
-        'thresholds' => [
-            'docBlock' => 90,
-            'regular' => 5,
-            'todo' => 5,
-            'fixme' => 5,
-            'missingDocBlock' => 10,
-            'Com/LoC' => 0.1,
-            'CDS' => 0.1,
-        ],
-        'only' => [
-            'missingDocblock'
-        ],
-        'output' => [
-            'type' => 'console', // "console" or 'html'
-            'file' => 'output.html', // file path for HTML output
-        ],
-    ];
+    private const CONFIG = <<<PHP
+<?php
 
+return [
+    'directories' => [
+        'src',
+    ],
+    'exclude' => [
+        'src/DTO',
+    ],
+    'thresholds' => [
+        'docBlock' => 90,
+        'regular' => 5,
+        'todo' => 5,
+        'fixme' => 5,
+        'missingDocBlock' => 10,
+        'Com/LoC' => 0.1,
+        'CDS' => 0.1,
+    ],
+    'only' => [
+        'missingDocblock'
+    ],
+    'output' => [
+        'type' => 'console', // "console" or 'html'
+        'file' => 'output.html', // file path for HTML output
+    ],
+];
+
+PHP;
     /**
      * @SuppressWarnings(PHPMD.ShortVariable)
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
