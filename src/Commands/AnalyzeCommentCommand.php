f8ecaca51e46ff8b851027f120939f791f5585df
Mikhail <salazar290720035017@gmail.com>
2024-07-14 13:40:40 +0300

    refactor: analyze comments command

--- src/Commands/AnalyzeCommentCommand.php
+++ src/Commands/AnalyzeCommentCommand.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\Commands;
 
-use SavinMikhail\CommentsDensity\AnalyzerFactory;
+use SavinMikhail\CommentsDensity\Analyzer\AnalyzerFactory;
+use SavinMikhail\CommentsDensity\Baseline\BaselineManager;
+use SavinMikhail\CommentsDensity\Baseline\Storage\SimplePhpBaselineStorage;
+use SavinMikhail\CommentsDensity\Baseline\Storage\SQLiteBaselineStorage;
+use SavinMikhail\CommentsDensity\Baseline\Storage\TreePhpBaselineStorage;
 use SavinMikhail\CommentsDensity\Reporters\ReporterFactory;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AnalyzeCommentCommand extends Command
@@ -16,6 +21,7 @@ class AnalyzeCommentCommand extends Command
     {
         $this->setName('analyze:comments')
             ->setDescription('Analyzes the comment density in files within a directory.')
+            ->addOption('storage', 's', InputOption::VALUE_REQUIRED, 'The type of storage to use (sqlite, tree, simple)', 'sqlite')
             ->setHelp('This command allows you to analyze the comments in PHP files within a specified directory.');
     }
 
@@ -24,13 +30,28 @@ class AnalyzeCommentCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $storageType = $input->getOption('storage');
+        $path = __DIR__ . '/../../baseline';
+
+        $storage = match ($storageType) {
+            'tree' => new TreePhpBaselineStorage(),
+            'simple' => new SimplePhpBaselineStorage(),
+            default => new SQLiteBaselineStorage(),
+        };
+        $extension = match ($storageType) {
+            'tree', 'simple' => 'php',
+            default => 'sqlite',
+        };
+
+        $baselineManager = (new BaselineManager($storage))->init($path . '.' . $extension);
+
         $configDto = $this->getConfigDto();
 
         $files = $this->getFilesFromDirectories($configDto->directories);
         $reporter = (new ReporterFactory())->createReporter($output, $configDto);
         $analyzerFactory = new AnalyzerFactory();
 
-        $analyzer = $analyzerFactory->getAnalyzer($configDto, $output);
+        $analyzer = $analyzerFactory->getAnalyzer($configDto, $output, $baselineManager);
 
         $outputDTO = $analyzer->analyze($files);
 
