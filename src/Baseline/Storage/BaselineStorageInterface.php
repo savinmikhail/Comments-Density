0708e322ce713a939241c7af7f172e299345f2be
Mikhail <salazar290720035017@gmail.com>
2024-07-14 15:19:21 +0300

    fix: storage interface

--- src/Baseline/Storage/BaselineStorageInterface.php
+++ src/Baseline/Storage/BaselineStorageInterface.php
@@ -6,7 +6,6 @@ namespace SavinMikhail\CommentsDensity\Baseline\Storage;
 
 interface BaselineStorageInterface
 {
-    public function init(string $path): void;
     public function setComments(array $comments): void;
     public function filterComments(array $comments): array;
 }
