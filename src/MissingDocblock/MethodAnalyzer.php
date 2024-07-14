37572e2985c06df792a6c2ce549775ef8e4f18f9
Mikhail <salazar290720035017@gmail.com>
2024-07-14 10:16:30 +0300

    feat: method analyzer

---
+++ src/MissingDocblock/MethodAnalyzer.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\CommentsDensity\MissingDocblock;
+
+use ArrayAccess;
+use Iterator;
+use PhpParser\Node;
+use PhpParser\Node\ComplexType;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\NodeTraverser;
+use ReflectionClass;
+
+use function class_exists;
+use function in_array;
+
+final readonly class MethodAnalyzer
+{
+    public function __construct(
+    ) {
+    }
+
+    public function methodNeedsGeneric(Node $node): bool
+    {
+        $returnType = $node->getReturnType();
+
+        if ($returnType === null) {
+            return false;
+        }
+
+        if (
+            $returnType instanceof Identifier
+            && in_array($returnType->toString(), ['array', 'iterable'], true)
+        ) {
+            return $this->arrayElementsHaveConsistentTypes($node);
+        }
+
+        if (
+            $returnType instanceof Name
+            && in_array($returnType->toString(), ['Generator', 'Traversable', 'Iterator', 'ArrayAccess'], true)
+        ) {
+            return $this->arrayElementsHaveConsistentTypes($node);
+        }
+
+        if ($this->isReturnClassTraversable($returnType)) {
+            return $this->arrayElementsHaveConsistentTypes($node);
+        }
+
+        return false;
+    }
+
+    private function isReturnClassTraversable(ComplexType|Identifier|Name $returnType): bool
+    {
+        if (!($returnType instanceof Name)) {
+            return false;
+        }
+
+        $returnTypeName = $returnType->toString();
+        if (!class_exists($returnTypeName)) {
+            return false;
+        }
+        $reflectionClass = new ReflectionClass($returnTypeName);
+        if (
+            ! $reflectionClass->implementsInterface(Iterator::class)
+            && !$reflectionClass->implementsInterface(ArrayAccess::class)
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    private function arrayElementsHaveConsistentTypes(Node $node): bool
+    {
+        $traverser = new NodeTraverser();
+        $visitor = new MissingGenericVisitor();
+
+        $traverser->addVisitor($visitor);
+        $traverser->traverse([$node]);
+
+        return $visitor->hasConsistentTypes;
+    }
+
+    public function methodThrowsUncaughtExceptions(Node $node): bool
+    {
+        $traverser = new NodeTraverser();
+        $visitor = new UncaughtExceptionVisitor();
+
+        $traverser->addVisitor($visitor);
+        $traverser->traverse([$node]);
+
+        return $visitor->hasUncaughtThrows;
+    }
+}
