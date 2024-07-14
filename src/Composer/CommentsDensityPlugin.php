0a3ee1c5a8806c129931f5a0941f582a91cb42e8
Mikhail <salazar290720035017@gmail.com>
2024-07-14 11:02:10 +0300

    fix: default config

--- src/Composer/CommentsDensityPlugin.php
+++ src/Composer/CommentsDensityPlugin.php
@@ -23,38 +23,40 @@ final class CommentsDensityPlugin implements PluginInterface, EventSubscriberInt
 
 return [
     'directories' => [
-        'src',
+        'src', // Directories to be scanned for comments
     ],
     'exclude' => [
-        'src/DTO',
+        'src/DTO', // Directories to be ignored during scanning
     ],
     'thresholds' => [
-        'docBlock' => 90,
+        // Limit occurrences of each comment type
+        'docBlock' => 90, 
         'regular' => 5,
         'todo' => 5,
         'fixme' => 5,
         'missingDocBlock' => 10,
-        'Com/LoC' => 0.1,
-        'CDS' => 0.1,
+        // Additional metrics thresholds
+        'Com/LoC' => 0.1, // Comments per Lines of Code
+        'CDS' => 0.1, // Comment Density Score
     ],
     'only' => [
-        'missingDocblock'
+        'missingDocblock', // Only this type will be analyzed; set to empty array for full statistics
     ],
     'output' => [
-        'type' => 'console', // "console" or 'html'
-        'file' => 'output.html', // file path for HTML output
+        'type' => 'console', // Supported values: 'console', 'html'
+        'file' => 'output.html', // File path for HTML output (only used if type is 'html')
     ],
     'missingDocblock' => [
-        'class' => true,
-        'interface' => true,
-        'trait' => true,
-        'enum' => true,
-        'property' => true,
-        'constant' => true,
-        'function' => true,
-        'requireForAllMethods' => true,
+        'class' => true, // Check for missing docblocks in classes
+        'interface' => true, // Check for missing docblocks in interfaces
+        'trait' => true, // Check for missing docblocks in traits
+        'enum' => true, // Check for missing docblocks in enums
+        'property' => true, // Check for missing docblocks in properties
+        'constant' => true, // Check for missing docblocks in constants
+        'function' => true, // Check for missing docblocks in functions
+        'requireForAllMethods' => true, // If false, only methods where @throws tag or generic can be applied will be checked
     ],
-    'use_baseline' => true,
+    'use_baseline' => true, // Filter collected comments against the baseline stored in comments_density.sqlite
 ];
 
 PHP;
