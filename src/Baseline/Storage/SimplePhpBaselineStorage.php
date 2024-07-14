89c727a89de18d487ce8d98ceab87ef14a307c6d
Mikhail <salazar290720035017@gmail.com>
2024-07-14 14:50:24 +0300

    refactor: simple storage

--- src/Baseline/Storage/SimplePhpBaselineStorage.php
+++ src/Baseline/Storage/SimplePhpBaselineStorage.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\Baseline\Storage;
 
-use SavinMikhail\CommentsDensity\DTO\Output\CommentDTO;
-
 final class SimplePhpBaselineStorage implements BaselineStorageInterface
 {
     private string $path;
@@ -22,20 +20,53 @@ final class SimplePhpBaselineStorage implements BaselineStorageInterface
 
     public function setComments(array $comments): void
     {
-        foreach ($comments as $comment) {
-            $this->baselineData[$comment->file . ':' . $comment->line] = [
-                'comment' => $comment->content,
-                'type' => $comment->commentType,
-            ];
+        // Clear the file content to ensure it's empty before adding new comments
+        unlink($this->path);
+        $this->init($this->path);
+
+        // Read existing data
+        $this->baselineData = include $this->path;
+
+        // Open the file for appending
+        $fileHandle = fopen($this->path, 'c+');
+
+        if ($fileHandle === false) {
+            throw new \RuntimeException("Unable to open file: " . $this->path);
         }
-        file_put_contents($this->path, "<?php return " . var_export($this->baselineData, true) . ";");
+
+        // Lock the file
+        if (flock($fileHandle, LOCK_EX)) {
+            // Move to the end of the file before the closing bracket
+            fseek($fileHandle, -2, SEEK_END);
+
+            foreach ($comments as $comment) {
+                $key = $comment->file . ':' . $comment->line;
+                $data = [
+                    'comment' => $comment->content,
+                    'type' => $comment->commentType,
+                ];
+
+                if (!isset($this->baselineData[$key])) {
+                    // Append the new comment to the file
+                    $entry = var_export($key, true) . " => " . var_export($data, true) . ",\n";
+                    fwrite($fileHandle, $entry);
+                    $this->baselineData[$key] = $data;
+                }
+            }
+
+            // Close the PHP array
+            fwrite($fileHandle, "];");
+            flock($fileHandle, LOCK_UN);
+        }
+
+        fclose($fileHandle);
     }
 
     public function filterComments(array $comments): array
     {
         return array_filter(
             $comments,
-            fn(CommentDTO $comment) => !isset($this->baselineData[$comment->file . ':' . $comment->line])
+            fn(array $comment) => !isset($this->baselineData[$comment['file'] . ':' . $comment['line']])
         );
     }
 }
