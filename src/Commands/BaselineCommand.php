f353695fbb81d2d34121a0bcb84223154b8bca2f
Mikhail <salazar290720035017@gmail.com>
2024-07-14 14:50:47 +0300

    refactor: baseline command

--- src/Commands/BaselineCommand.php
+++ src/Commands/BaselineCommand.php
@@ -6,12 +6,9 @@ namespace SavinMikhail\CommentsDensity\Commands;
 
 use SavinMikhail\CommentsDensity\Analyzer\AnalyzerFactory;
 use SavinMikhail\CommentsDensity\Baseline\BaselineManager;
-use SavinMikhail\CommentsDensity\Baseline\Storage\SimplePhpBaselineStorage;
-use SavinMikhail\CommentsDensity\Baseline\Storage\SQLiteBaselineStorage;
 use SavinMikhail\CommentsDensity\Baseline\Storage\TreePhpBaselineStorage;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class BaselineCommand extends Command
@@ -20,26 +17,16 @@ final class BaselineCommand extends Command
     {
         $this->setName('generate:baseline')
             ->setDescription('Generate a baseline of comments to ignore them in the future')
-            ->addOption('storage', 's', InputOption::VALUE_REQUIRED, 'The type of storage to use (sqlite, tree, simple)', 'sqlite')
             ->setHelp('This command allows you to ignore old tech debt and start this quality check from this point');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $storageType = $input->getOption('storage');
-        $path = __DIR__ . '/../../baseline';
+        $path = __DIR__ . '/../../baseline.php';
 
-        $storage = match ($storageType) {
-            'tree' => new TreePhpBaselineStorage(),
-            'simple' => new SimplePhpBaselineStorage(),
-            default => new SQLiteBaselineStorage(),
-        };
-        $extension = match ($storageType) {
-            'tree', 'simple' => 'php',
-            default => 'sqlite',
-        };
+        $storage =  new TreePhpBaselineStorage();
 
-        $baselineManager = (new BaselineManager($storage))->init($path . '.' . $extension);
+        $baselineManager = (new BaselineManager($storage))->init($path);
 
         $configDto = $this->getConfigDto();
 
