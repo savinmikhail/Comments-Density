8bc4a049b1eea0536467d203b7e23b2262facd09
Mikhail <salazar290720035017@gmail.com>
2024-07-14 08:47:56 +0300

    feat: baseline command

---
+++ src/Commands/BaselineCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\CommentsDensity\Commands;
+
+use SavinMikhail\CommentsDensity\AnalyzerFactory;
+use SavinMikhail\CommentsDensity\BaselineManager;
+use SavinMikhail\CommentsDensity\Database\SQLiteDatabaseManager;
+use SavinMikhail\CommentsDensity\Reporters\ReporterFactory;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use function file_exists;
+use function touch;
+
+final class BaselineCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('generate:baseline')
+            ->setDescription('Generate a baseline of comments to ignore them in the future')
+            ->setHelp('This command allows you to ignore old tech debt and start this quality check from this point');
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $databaseFile = __DIR__ . '/../../comments_density.sqlite';
+        if (! file_exists($databaseFile)) {
+            touch($databaseFile);
+        }
+        $dbManager = new SQLiteDatabaseManager($databaseFile);
+        $connection = $dbManager->getConnection();
+        $baselineManager = new BaselineManager($connection);
+
+        $configDto = $this->getConfigDto();
+
+        $files = $this->getFilesFromDirectories($configDto->directories);
+        $analyzerFactory = new AnalyzerFactory();
+
+        $analyzer = $analyzerFactory->getAnalyzer($configDto, $output);
+        $outputDTO = $analyzer->analyze($files);
+
+        $baselineManager->set($outputDTO);
+
+        $output->writeln('<info>Baseline generated successfully!</info>');
+
+        return SymfonyCommand::SUCCESS;
+    }
+}
