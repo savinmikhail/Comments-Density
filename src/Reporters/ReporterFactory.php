ffce6ee24a2d7bd7d3733c839c4ca7bd4b215d57
Mikhail <salazar290720035017@gmail.com>
2024-07-21 17:41:41 +0300

    fix: reporter factory

--- src/Reporters/ReporterFactory.php
+++ src/Reporters/ReporterFactory.php
@@ -11,11 +11,8 @@ final readonly class ReporterFactory
 {
     public function createReporter(OutputInterface $output, ConfigDTO $configDto): ReporterInterface
     {
-        if (empty($configDto->output)) {
-            return new ConsoleReporter($output);
-        }
-        if ($configDto->output['type'] === 'html') {
-            return new HtmlReporter($configDto->output['file']);
+        if ($configDto->output->type === 'html') {
+            return new HtmlReporter($configDto->output->file);
         }
         return new ConsoleReporter($output);
     }
