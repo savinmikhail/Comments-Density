de264e395bccf2537db60005944e166e81e74234
Mikhail <salazar290720035017@gmail.com>
2024-07-21 17:42:37 +0300

    feat: output dto

---
+++ src/DTO/Input/OutputDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\CommentsDensity\DTO\Input;
+
+final readonly class OutputDTO
+{
+    public function __construct(
+        public string $type,
+        public string $file,
+    ) {
+    }
+}
