14efe5c602bd2d51f741e2803d5feb589fd427a0
Mikhail <salazar290720035017@gmail.com>
2024-07-21 21:35:43 +0300

    refactor: storage interface

--- src/Baseline/Storage/BaselineStorageInterface.php
+++ src/Baseline/Storage/BaselineStorageInterface.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\Baseline\Storage;
 
+use SavinMikhail\CommentsDensity\DTO\Output\CommentDTO;
+
 interface BaselineStorageInterface
 {
+    /**
+     * @param CommentDTO[] $comments
+     * @return void
+     */
     public function setComments(array $comments): void;
+
+    /**
+     * @param CommentDTO[] $comments
+     * @return CommentDTO[]
+     */
     public function filterComments(array $comments): array;
 }
