770bd7629dc441386e00a46da705c521b9878353
Mikhail <salazar290720035017@gmail.com>
2024-07-21 17:41:12 +0300

    feat: output conf dto

--- src/DTO/Input/ConfigDTO.php
+++ src/DTO/Input/ConfigDTO.php
@@ -6,10 +6,19 @@ namespace SavinMikhail\CommentsDensity\DTO\Input;
 
 final readonly class ConfigDTO
 {
+    /**
+     * @param array<string, float> $thresholds
+     * @param string[] $exclude
+     * @param OutputDTO $output
+     * @param string[] $directories
+     * @param string[] $only
+     * @param MissingDocblockConfigDTO $docblockConfigDTO
+     * @param bool $useBaseline
+     */
     public function __construct(
         public array $thresholds,
         public array $exclude,
-        public array $output,
+        public OutputDTO $output,
         public array $directories,
         public array $only,
         public MissingDocblockConfigDTO $docblockConfigDTO,
