43bfcc72439588b5fd9113ce6e739e7d3d05faa8
Mikhail <salazar290720035017@gmail.com>
2024-07-21 14:24:20 +0300

    feat: comment constant interface

---
+++ src/Comments/CommentConstantsInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SavinMikhail\CommentsDensity\Comments;
+
+interface CommentConstantsInterface
+{
+    public const PATTERN = '';
+    public const COLOR = '';
+    public const WEIGHT = 0.0;
+    public const COMPARISON_TYPE = '';
+    public const NAME = '';}
