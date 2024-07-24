aa1c080e43d66fa4f3ab1ae303c1a72ae1d58e3e
Mikhail <salazar290720035017@gmail.com>
2024-07-24 08:45:27 +0300

    refactor: mdb visitor imports

--- src/MissingDocblock/MissingDocBlockVisitor.php
+++ src/MissingDocblock/MissingDocBlockVisitor.php
@@ -4,28 +4,9 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\MissingDocblock;
 
-use ArrayAccess;
-use Iterator;
 use PhpParser\Node;
-use PhpParser\Node\ComplexType;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name;
-use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\ClassConst;
-use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Enum_;
-use PhpParser\Node\Stmt\Function_;
-use PhpParser\Node\Stmt\Interface_;
-use PhpParser\Node\Stmt\Property;
-use PhpParser\Node\Stmt\Trait_;
-use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
-use ReflectionClass;
-use SavinMikhail\CommentsDensity\DTO\Input\MissingDocblockConfigDTO;
-
 use SavinMikhail\CommentsDensity\DTO\Output\CommentDTO;
-use function class_exists;
-use function in_array;
 
 final class MissingDocBlockVisitor extends NodeVisitorAbstract
 {
