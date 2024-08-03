e0dd3d528e9c9c6985b018319a19579fdd83963c
Mikhail <salazar290720035017@gmail.com>
2024-08-03 09:04:38 +0300

    fix: check parents for iterable interfaces

--- src/MissingDocblock/MethodAnalyzer.php
+++ src/MissingDocblock/MethodAnalyzer.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace SavinMikhail\CommentsDensity\MissingDocblock;
 
+use ArrayAccess;
+use Iterator;
 use PhpParser\Node;
 use PhpParser\Node\ComplexType;
 use PhpParser\Node\Identifier;
@@ -11,11 +13,14 @@ use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PhpParser\NodeTraverser;
+use ReflectionClass;
+use Traversable;
 
 use function class_exists;
 use function in_array;
+use function interface_exists;
 
-final readonly class MethodAnalyzer
+final class MethodAnalyzer
 {
     public function methodNeedsGeneric(ClassMethod|Function_ $node): bool
     {
@@ -54,23 +59,44 @@ final readonly class MethodAnalyzer
             return true;
         }
 
-        return $this->isClassTraversable($type);
+        return $this->isClassOrInterfaceTraversable($type);
     }
 
-    private function isClassTraversable(ComplexType|Identifier|Name $returnType): bool
+    private function isClassOrInterfaceTraversable(ComplexType|Identifier|Name $type): bool
     {
-        if (!($returnType instanceof Name)) {
+        if (!($type instanceof Name)) {
             return false;
         }
 
-        $returnTypeName = $returnType->toString();
-        if (!class_exists($returnTypeName)) {
+        $typeName = $type->toString();
+
+        if (!class_exists($typeName) && !interface_exists($typeName)) {
             return false;
         }
 
-        $reflectionClass = new \ReflectionClass($returnTypeName);
-        return $reflectionClass->implementsInterface(\Iterator::class)
-            || $reflectionClass->implementsInterface(\ArrayAccess::class);
+        return $this->isTraversableRecursively(new ReflectionClass($typeName));
+    }
+
+    private function isTraversableRecursively(ReflectionClass $reflection): bool
+    {
+        if ($reflection->implementsInterface(Iterator::class)
+            || $reflection->implementsInterface(ArrayAccess::class)
+            || $reflection->implementsInterface(Traversable::class)) {
+            return true;
+        }
+
+        foreach ($reflection->getInterfaces() as $interface) {
+            if ($this->isTraversableRecursively($interface)) {
+                return true;
+            }
+        }
+
+        $parentClass = $reflection->getParentClass();
+        if ($parentClass !== false) {
+            return $this->isTraversableRecursively($parentClass);
+        }
+
+        return false;
     }
 
     public function methodThrowsUncaughtExceptions(Node $node): bool
