5832e550bf84355d9cc18409ddcb6c2387ceb167
Mikhail <salazar290720035017@gmail.com>
2024-08-04 15:57:19 +0300

    test: templated class

---
+++ tests/TestFiles/TemplatedClass.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SavinMikhail\Tests\CommentsDensity\TestFiles;
+
+/**
+ * @template T of object
+ */
+final readonly class TemplatedClass
+{
+    public function __construct()
+    {
+    }
+}
