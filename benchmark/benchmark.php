5a08b83c58fedfc73c9c75bd503ad77a5a16bcb7
Mikhail <salazar290720035017@gmail.com>
2024-08-18 20:45:07 +0700

    php-cs-fixer benchmark php

--- benchmark/benchmark.php
+++ benchmark/benchmark.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 $iterations = 10;
 $times = [];
 $memories = [];
 
-for ($i = 0; $i < $iterations; $i++) {
+for ($i = 0; $i < $iterations; ++$i) {
     $startTime = microtime(true);
     $startMemory = memory_get_usage();
 
@@ -13,17 +15,16 @@ for ($i = 0; $i < $iterations; $i++) {
     $endTime = microtime(true);
     $endMemory = memory_get_usage();
 
-
     $times[] = $endTime - $startTime;
     $memories[] = $endMemory - $startMemory;
 
     if ($returnVar !== 0) {
-        echo "Command failed with status: $returnVar" . PHP_EOL;
+        echo "Command failed with status: {$returnVar}" . PHP_EOL;
     }
 }
 
 $averageTime = array_sum($times) / $iterations;
 $averageMemory = array_sum($memories) / $iterations;
 
-echo "Average execution time: " . ($averageTime * 1000) . " ms" . PHP_EOL;
-echo "Average memory usage: " . ($averageMemory / 1024 / 1024) . " MB" . PHP_EOL;
+echo 'Average execution time: ' . ($averageTime * 1000) . ' ms' . PHP_EOL;
+echo 'Average memory usage: ' . ($averageMemory / 1024 / 1024) . ' MB' . PHP_EOL;
