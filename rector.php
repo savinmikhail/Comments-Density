3971f4e7bf7a0e6db08cb8a36c77e3941b379b03
Mikhail <salazar290720035017@gmail.com>
2024-08-18 20:19:54 +0700

    feat: rector rules for refactoring

--- rector.php
+++ rector.php
@@ -3,11 +3,19 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([
+        __DIR__ . '/bin',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    ->withSets([DowngradeLevelSetList::DOWN_TO_PHP_74]);
+    ->withParallel()
+    ->withCache(__DIR__ . '/var/rector')
+    ->withPhpSets(php83: true)
+    ->withSkip([
+        StringableForToStringRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ]);
