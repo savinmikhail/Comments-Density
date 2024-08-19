07b17e7cafa22b66d46a0a0b2d813ff19e12e54c
Mikhail <salazar290720035017@gmail.com>
2024-08-19 13:19:46 +0700

    cs-fixer update rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,7 +7,12 @@ use PhpCsFixer\Finder;
 use PHPyh\CodingStandard\PhpCsFixerCodingStandard;
 
 $finder = (new Finder())
-    ->in(['src', 'tests', 'bin', 'benchmark'])
+    ->in([
+        'src',
+        // 'tests',
+        'bin',
+        'benchmark',
+    ])
     ->append([
         __FILE__,
     ]);
@@ -16,6 +21,14 @@ $config = (new Config())
     ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache')
     ->setFinder($finder);
 
-(new PhpCsFixerCodingStandard())->applyTo($config);
+(new PhpCsFixerCodingStandard())->applyTo($config, [
+    'global_namespace_import' => [
+        'import_constants' => true,
+        'import_functions' => true,
+        'import_classes' => true,
+    ],
+    'php_unit_test_case_static_method_calls' => ['call_type' => 'this'],
+    'blank_line_between_import_groups' => true,
+]);
 
 return $config;
