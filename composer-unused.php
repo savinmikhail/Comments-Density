9624f192364a0fd151b37d2b1d24377d157c374f
Mikhail <salazar290720035017@gmail.com>
2025-01-03 11:54:40 +0700

    remove unused packages

---
+++ composer-unused.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use ComposerUnused\ComposerUnused\Configuration\Configuration;
+use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
+
+return static fn(Configuration $config): Configuration => $config
+    ->addNamedFilter(NamedFilter::fromString('composer/xdebug-handler'));
