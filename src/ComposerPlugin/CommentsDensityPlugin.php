0a61f6cf18ee0da6c8da1257dc39cca0e9735466
Mikhail <salazar290720035017@gmail.com>
2025-01-04 11:24:48 +0700

    update composer plugin config

--- src/ComposerPlugin/CommentsDensityPlugin.php
+++ src/ComposerPlugin/CommentsDensityPlugin.php
@@ -17,45 +17,33 @@ final class CommentsDensityPlugin implements PluginInterface, EventSubscriberInt
 {
     private const CONFIG = <<<'PHP'
         <?php
-
-        return [
-            'directories' => [
-                'src', // Directories to be scanned for comments
-            ],
-            'exclude' => [
-                'src/DTO', // Directories to be ignored during scanning
-            ],
-            'thresholds' => [
-                // Limit occurrences of each comment type
-                'docBlock' => 90, 
+        
+        declare(strict_types=1);
+        
+        use SavinMikhail\CommentsDensity\AnalyzeComments\Config\DTO\Config;
+        use SavinMikhail\CommentsDensity\AnalyzeComments\Config\DTO\ConsoleOutputDTO;
+        use SavinMikhail\CommentsDensity\AnalyzeComments\Config\DTO\MissingDocblockConfigDTO;
+        
+        return new Config(
+            thresholds: [
+                'docBlock' => 1000,
                 'regular' => 5,
-                'todo' => 5,
+                'todo' => 1,
                 'fixme' => 5,
-                'missingDocBlock' => 10,
-                // Additional metrics thresholds
-                'Com/LoC' => 0.1, // Comments per Lines of Code
-                'CDS' => 0.1, // Comment Density Score
+                'missingDocBlock' => 1,
+                'Com/LoC' => 0.1,
+                'CDS' => 0.1,
             ],
-            'only' => [
-                'missingDocblock', // Only this type will be analyzed; set to empty array for full statistics
+            exclude: [
+                'src/DTO',
             ],
-            'output' => [
-                'type' => 'console', // Supported values: 'console', 'html'
-                'file' => 'output.html', // File path for HTML output (only used if type is 'html')
+            output: ConsoleOutputDTO::create(),
+            directories: [
+                'src',
             ],
-            'missingDocblock' => [
-                'class' => true, // Check for missing docblocks in classes
-                'interface' => true, // Check for missing docblocks in interfaces
-                'trait' => true, // Check for missing docblocks in traits
-                'enum' => true, // Check for missing docblocks in enums
-                'property' => true, // Check for missing docblocks in properties
-                'constant' => true, // Check for missing docblocks in constants
-                'function' => true, // Check for missing docblocks in functions
-                 // If false, only methods where @throws tag or generic can be applied will be checked
-                'requireForAllMethods' => true,
-            ],
-            'use_baseline' => true, // Filter collected comments against the baseline stored in baseline.php
-        ];
+            docblockConfigDTO: new MissingDocblockConfigDTO(class: true),
+            disable: []
+        );
 
         PHP;
 
