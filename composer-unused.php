c9255bce9f8249231a086d9fc2c56ed230ed240b
Mikhail <salazar290720035017@gmail.com>
2025-01-04 13:36:25 +0700

    remove zombie package

--- composer-unused.php
+++ composer-unused.php
@@ -5,5 +5,4 @@ declare(strict_types=1);
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
 use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 
-return static fn(Configuration $config): Configuration => $config
-    ->addNamedFilter(NamedFilter::fromString('composer/xdebug-handler'));
+return static fn(Configuration $config): Configuration => $config;
